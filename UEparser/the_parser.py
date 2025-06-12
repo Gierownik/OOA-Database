4aa416bb77f70232cc67e47df6a6a91a07ff848b
Gierownik <krzysztoft008@gmail.com>
2025-06-12 23:48:51 +0200

    Weapons DB

--- UEparser/the_parser.py
+++ UEparser/the_parser.py
@@ -164,3 +164,89 @@ for skill_name, data in skill_rows.items():
 with open("devices_db_test.json", "w", encoding="utf-8") as out_file:
     json.dump(device_output, out_file, indent=2, ensure_ascii=False)
 print("Device database shit workin")
+#-------------------------------------------Goons---------------------------------------
+weapon_output = []
+
+for skill_name, data in skill_rows.items():
+    weapon_enum = data.get("WeaponID_22_BB8DC4A4405B740F189E2AAEF93AA65E")
+    if not weapon_enum or weapon_enum not in weapon_id_to_name:
+        continue
+
+    weapon_name = weapon_id_to_name[weapon_enum]
+
+    req = data.get("Requirements_8_A4C8470C4FCFFF82BFB0F097CA1EC92B", {})
+    body = req.get("Body_10_68FBC6C34B6DDB19E010A9AB2419B88B", 0)
+    tech = req.get("Tech_11_D9A98AA74B87F1DD6B0F43B4233753BC", 0)
+    hardware = req.get("Hardware_12_9310D7DB447E651E58D0268CC41AC3B7", 0)
+
+    weapon_stat = weapon_stats.get(skill_name, {})
+    damage_section = weapon_stat.get("damage_51_4503C2744F2DD64F4FC8FFADCC5F09EE", {})
+
+    slot_enum = weapon_stat.get("weaponClass_103_A033AA0D4A67345BE9F09089017236C0", "")
+    cat_enum = weapon_stat.get("category_158_45CB73F443BFECF1A75A4ABAD13BEB27", "")
+    fire_enum = weapon_stat.get("fireMode_27_09F1BA2744EAF4289A2295991B702E2A", "")
+
+    slot_key = slot_enum.replace("ENUM_WeaponClass::", "")
+    cat_key = cat_enum.replace("ENUM_Category::", "")
+    fire_key = fire_enum.replace("ENUM_FireMode::", "")
+
+    slot_name = weapon_class_to_value.get(slot_key, slot_key)
+    cat_name = weapon_category_to_value.get(cat_key, cat_key)
+    fire_name = weapon_mode_to_value.get(fire_key, fire_key)
+
+    stats = {
+        "ID": weapon_stat.get("ID_164_1DF0FD0E430EA7E965B389963917762B", 0),
+        "Weapon Type": weapon_stat.get("Class_165_71C2F5264EA07AE77DDE398BD5729CD0", 0),
+        "Slot": slot_name,
+        "Category": cat_name,
+        "Firemode": fire_name,
+        "Firerate": f"{weapon_stat.get('roundsPerMinute_88_9A62D92546A54AF5B9BD9CA681C99779', 0)} rpm",
+        "Near Damage": damage_section.get("DamageNear_28_C0ECF144455E9C9F13D929A6E1A9FA6C", 0),
+        "Far Damage": damage_section.get("DamageFar_31_9A92287545630DA083AAA18D6F6D52B2", 0),
+        "Range": f"{damage_section.get('RangeNear_32_7051C33A45ED3E81881748AF79D60E71', 0)} - {damage_section.get('RangeFar_33_9FC06C8A49BBBCDC074BDEBAF8F978BB', 0)} m",
+        "Headshot Multiplier": f"{damage_section.get('HeadMultiplier_18_6AC23285474D12C7B9B9C9B43C915FE8', 0)}x",
+        "Limb Multiplier": f"{damage_section.get('LimbMultiplier_36_F099D86241C4A7BDF8FC47849F76F552', 0)}x",
+        "Penetration": f"{weapon_stat.get('penetrationPower_18_B8E8BDFC45F3DFA9B84410AEEF7CF3DE', 0)} cm",
+        "Velocity": f"{weapon_stat.get('muzzleVelocity_20_0A3F52FD4DB2E8D23F8E05A26A11E7BA', 0) / 100} m/s",
+        "Vertical Recoil": weapon_stat.get("recoilVertical_14_8D17013D41FC3D7BB68C2E816F8B69C7", 0),
+        "Horizontal Recoil": weapon_stat.get("recoilHorizonal_16_562990B74D7D4F4DDCF429942D1839C5", 0),
+        "Time to ADS": f"{weapon_stat.get('aimTime_34_F110FBC149382F201D7A3C9AA8BE8D17', 0)}s",
+        "Hipfire Spread": f"{weapon_stat.get('hipSpread_92_F512DB8F4AA38F47776755B509674FDB', 0)}°",
+        "ADS Spread": f"{weapon_stat.get('aimSpread_93_4DB54D3B45DEEA9F6DBF20B24D661F94', 0)}°",
+        "Spread Gain": f"{weapon_stat.get('spreadGain_135_D194D65F441D7033F8F0E89B097BD9ED', 0)}°",
+        "Speed Penalty": f"0.{weapon_stat.get('speedModifier_75_CA08E44842050D047DB75CA31280BE88', 0)} m/s"
+    }
+
+    if (val := weapon_stat.get("burstCount_125_4B49DFE84C24E8AA5A63B7B6983618D1")) not in [0, 0.0, False, ""]:
+        stats["Burst Count"] = val
+    if (val := weapon_stat.get("burstDelay_152_D2A7BF0742680DCB3E9BE381C99B9BFC")) not in [0, 0.0, False, ""]:
+        stats["Burst Delay"] = f"{val}s"
+    if (val := weapon_stat.get("spinUpDuration_114_42F529AD41ADDCCCDB0ED0915E8BCCEC")) not in [0, 0.0, False, ""]:
+        stats["Spinup"] = f"{val}s"
+    if (val := weapon_stat.get("usesAmmo_109_C8A226E545A12A663A1AF398568E1897")) not in [0, 0.0, True, ""]:
+        stats["Infinite Ammo"] = True
+    if (val := weapon_stat.get("ammoCapacity_37_C66372EB4769BC9D909A6CA771FD33A0")) not in [0, 0.0, False, ""]:
+        stats["Ammo Capacity"] = val
+    if (val := weapon_stat.get("reloadSpeed_42_4B9E289B467B258218AE298E52A1B0E3")) not in [0, 0.0, False, ""]:
+        stats["Reload Duration"] = f"{val}s"
+    if (val := weapon_stat.get("rechamberDuration_121_DA9C2951452609B501BB76B185DE3800")) not in [0, 0.0, False, ""]:
+        stats["Rechamber Duration"] = f"{val}s"
+    if (val := weapon_stat.get("singleReload_128_E69294BE4B79448A0BAC2AB62561973D")) not in [0, 0.0, False, ""]:
+        stats["One by One Reload"] = True
+    if (val := damage_section.get("BleedMultiplier_27_0D5F760E45CA9912594F6787BD30615F")) not in [1, 1.0, False, ""]:
+        stats["Bleed Multiplier"] = f"{val}x"
+
+    weapon_output.append({
+        "name": weapon_name,
+        "stats": stats,
+        "foundations": {
+            "body": body,
+            "tech": tech,
+            "hardware": hardware
+        }
+    })
+
+with open("weapons_db_test.json", "w", encoding="utf-8") as out_file:
+    json.dump(weapon_output, out_file, indent=2, ensure_ascii=False)
+
+print("Weapon database shit workin")
