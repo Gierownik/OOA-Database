f9377f4ec5184bd80a108a0bdf6aa71e54a395aa
Gierownik <krzysztoft008@gmail.com>
2025-08-01 23:22:13 +0200

    The Removal of the bot 1/2

--- UEparser/Quick Parse.bat
+++ UEparser/Quick Parse.bat
@@ -1,10 +1,4 @@
 @echo off
-echo Running ueparser.py
-python ueparser.py
-timeout /t 1 > nul
-echo Running UE-DC_Parser.py
-python UE-DC_Parser.py
-timeout /t 1 > nul
 echo Running the_parser.py
 python the_parser.py
 echo Running ID_Parse.py
